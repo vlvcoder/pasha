949de1d978ca60066dff5e7f50502c5c6a8986c6
vlvcoder <vlvcoder@gmail.com>
2026-07-26 01:29:34 +0300

    *trimmer

--- trimmer/main.js
+++ trimmer/main.js
@@ -6,6 +6,7 @@ const divTimer = document.getElementById('timer');
 const btnStart = document.getElementById('start');
 const digit1 = document.getElementById('digit1');
 const digit2 = document.getElementById('digit2');
+const scoreList = document.getElementById('scoreList');
 
 const { showDigit: showDigit1, setVisibility: setVisibility1 } = makeDigit(digit1);
 const { showDigit: showDigit2 } = makeDigit(digit2);
@@ -19,11 +20,13 @@ function setScoreFn(cnt) {
 
 function makeTrimmer(field, caret, timer, buttonStart, setScore) {
     const STEP = 40;
-    const WIDTH = 25;
-    const HEIGHT = 15;
-    const DURATION_SEC = 30;
+    const WIDTH = 15;
+    const HEIGHT = 10;
+    const DURATION_SEC = 60;
+    const TAIL_PADDING = 5;
 
     let point = [0, 0];
+    let tail = [];
     let targets = [];
     let count = 0;
     let timerSec = DURATION_SEC;
@@ -42,12 +45,13 @@ function makeTrimmer(field, caret, timer, buttonStart, setScore) {
         field.classList.remove('finished');
 
         point = [0, 0];
+        tail = [];
         targets = [];
         count = 0;
         timerSec = DURATION_SEC;
-        
+
         setScore(count);
-        
+
         placeCaret([0, 0]);
         insertTarget();
         intervalId = setInterval(onInterval, 1000);
@@ -64,25 +68,59 @@ function makeTrimmer(field, caret, timer, buttonStart, setScore) {
     });
 
     function insertTarget() {
+        if (tail.length >= WIDTH * HEIGHT - 2) {
+            finishGame();
+            return;
+        }
+
         const target = document.createElement('div');
         target.className = 'figure target';
         target.style.width = `${STEP}px`;
         target.style.height = `${STEP}px`;
-        target.style.left = `${random(WIDTH) * STEP}px`;
-        target.style.top = `${random(HEIGHT) * STEP}px`;
+        let targetPoint;
+        let isTargetEqualsPoint;
+        let isTargetEqualsTail;
+        do {
+            targetPoint = [random(WIDTH), random(HEIGHT)];
+            isTargetEqualsPoint = isPointsEqual(targetPoint, point);
+            isTargetEqualsTail = tail.find(({ tailPoint }) => isPointsEqual(targetPoint, tailPoint));
+        } while (isTargetEqualsPoint || isTargetEqualsTail);
+
+        target.style.left = `${targetPoint[0] * STEP}px`;
+        target.style.top = `${targetPoint[1] * STEP}px`;
         field.appendChild(target);
         targets.push(target);
     }
 
     function checkTargets() {
-        const completed = targets.find(target => point[0] === target.offsetLeft && point[1] === target.offsetTop);
+        const completed = targets
+            .find(target =>
+                point[0] === Math.round(target.offsetLeft / STEP)
+                && point[1] === Math.round(target.offsetTop / STEP));
         if (completed) {
             targets = targets.filter(target => target !== completed);
             completed.remove();
-            insertTarget();
             count++;
             setScore(count);
         }
+        return !!completed;
+    }
+
+    function finishGame() {
+        clearInterval(intervalId);
+        targets.forEach(target => target.remove());
+        targets = [];
+        tail.forEach(ti => ti.element.remove());
+        tail = [];
+        timerSec = 0;
+        caret.style.opacity = 0;
+        field.setAttribute('data-text', count);
+        field.classList.add('finished');
+        buttonStart.style.opacity = 1;
+
+        const li = document.createElement('li');
+        li.textContent = count;
+        scoreList.appendChild(li);
     }
 
     function onInterval() {
@@ -90,31 +128,79 @@ function makeTrimmer(field, caret, timer, buttonStart, setScore) {
         timer.textContent = String(timerSec);
 
         if (timerSec === 0) {
-            clearInterval(intervalId);
-            targets.forEach(target => target.remove());
-            targets = [];
-            caret.style.opacity = 0;
-            field.setAttribute('data-text', count);
-            field.classList.add('finished');
-            buttonStart.style.opacity = 1;
+            finishGame();
         }
     }
 
-    function placeCaret(diff) {
+    function placeCaret(diff, keyName) {
         const [x, y] = diff;
         const [pointX, pointY] = point;
+        const newPoint = [pointX + x, pointY + y];
 
         if (pointX + x < 0 || pointY + y < 0) {
             return;
         }
-        if (pointX + x >= field.clientWidth || pointY + y >= field.clientHeight) {
+        if (pointX + x >= WIDTH || pointY + y >= HEIGHT) {
             return;
         }
 
+        if (tail[0] && tail[0].tailPoint) {
+            if (isPointsEqual(tail[0].tailPoint, newPoint)) {
+                return;
+            }
+        }
+
+        if (tail.find(({ tailPoint }) => isPointsEqual(tailPoint, newPoint))) {
+            finishGame();
+            return;
+        }
+
+        clearDirections();
+        caret.classList.add(keyName);
+
+        const pervPoint = point;
         point = [pointX + x, pointY + y];
-        caret.style.left = `${point[0]}px`;
-        caret.style.top = `${point[1]}px`;
-        checkTargets();
+        caret.style.left = `${point[0] * STEP}px`;
+        caret.style.top = `${point[1] * STEP}px`;
+        const targetCompleted = checkTargets();
+        if (targetCompleted) {
+            insertTailItem(pervPoint);
+            insertTarget();
+        } else {
+            moveTail(pervPoint);
+        }
+    }
+
+    const placeTailElement = (tailelement, tailPoint) => {
+        const [left, right] = tailPoint;
+        tailelement.style.left = `${left * STEP + TAIL_PADDING}px`;
+        tailelement.style.top = `${right * STEP + TAIL_PADDING}px`;
+    };
+
+    function moveTail(tailPoint) {
+        if (tail.length === 0) {
+            return;
+        }
+        for (let i = tail.length - 1; i > 0; i--) {
+            tail[i].element.style.left = tail[i - 1].element.style.left;
+            tail[i].element.style.top = tail[i - 1].element.style.top;
+            tail[i].tailPoint = [...tail[i - 1].tailPoint];
+        }
+        tail[0].tailPoint = tailPoint;
+        placeTailElement(tail[0].element, tailPoint);
+    }
+
+    function insertTailItem(tailPoint) {
+        const element = document.createElement('div');
+        element.className = 'tail';
+        element.style.width = `${STEP - 2 * TAIL_PADDING}px`;
+        element.style.height = `${STEP - 2 * TAIL_PADDING}px`;
+        placeTailElement(element, tailPoint);
+        field.appendChild(element);
+        tail.unshift({
+            element,
+            tailPoint,
+        });
     }
 
     field.addEventListener('keydown', (event) => {
@@ -127,22 +213,19 @@ function makeTrimmer(field, caret, timer, buttonStart, setScore) {
         if (['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'].includes(keyName)) {
             switch (keyName) {
                 case 'ArrowUp':
-                    diff = [0, -STEP];
+                    diff = [0, -1];
                     break;
                 case 'ArrowDown':
-                    diff = [0, STEP];
+                    diff = [0, 1];
                     break;
                 case 'ArrowLeft':
-                    diff = [-STEP, 0];
+                    diff = [-1, 0];
                     break;
                 case 'ArrowRight':
-                    diff = [STEP, 0];
+                    diff = [1, 0];
                     break;
             }
-            clearDirections();
-            caret.classList.add(keyName);
-            placeCaret(diff);
-
+            placeCaret(diff, keyName);
             event.preventDefault();
             event.stopPropagation();
         }
@@ -159,4 +242,8 @@ function makeTrimmer(field, caret, timer, buttonStart, setScore) {
 
 function random(n) {
     return Math.floor(Math.random() * n);
+}
+
+function isPointsEqual(p1, p2) {
+    return p1[0] === p2[0] && p1[1] === p2[1];
 }
